37ece0fe3cbcda5621c76d72898f8fd5abad1bdd
Ganganathen <ganganathan.m@outlook.com>
2025-04-22 14:05:32 +0530

    Refactor Udo component: update state management for currentIndex and improve data handling logic

--- urdemo/src/app/finish/page.js
+++ urdemo/src/app/finish/page.js
@@ -10,7 +10,7 @@ export default function Udo() {
   const [currentIndex, setCurrentIndex] = useState(data.length - 1);
 
   useEffect(() => {
-    setCurrentIndex(data.length - 1);
+    setCurrentIndex(data.length);
   }, [data]);
 
   const handleData1 = () => {
@@ -19,11 +19,14 @@ export default function Udo() {
       alert("This field is required.");
       return;
     }
-    setData(prev => [...prev, { 
+    setData((prev) => [
+      ...prev,
+      {
         id: prev.length,
-        data1: val 
-    }]);
-    input2Ref.current.value = "";
+        data1: val,
+      },
+    ]);
+    input1Ref.current.value = "";
   };
 
   const handleData2 = () => {
@@ -32,15 +35,18 @@ export default function Udo() {
       alert("This field is required.");
       return;
     }
-    setData(prev => [...prev, { 
-        id: prev.length, 
-        data2: val 
-    }]);
-    input1Ref.current.value = "";
+    setData((prev) => [
+      ...prev,
+      {
+        id: prev.length,
+        data2: val,
+      },
+    ]);
+    input2Ref.current.value = "";
   };
 
   const handleUndo = () => {
-    const newIndex = currentIndex-1;
+    const newIndex = currentIndex - 1;
     if (newIndex >= 0) {
       setCurrentIndex(newIndex);
       if (data[newIndex].data1) {
@@ -69,20 +75,29 @@ export default function Udo() {
 
   return (
     <div>
-      <div className='mt-20 px-30'>
-        <input ref={input1Ref} className='border-2' type='text' />
-        <button className='border-2 px-1 mx-2' onClick={handleData1}>Save</button>
-        <br /><br />
-        <input ref={input2Ref} className='border-2' type='text' />
-        <button className='border-2 px-1 mx-2' onClick={handleData2}>Save</button>
+      <div className="mt-20 px-30">
+        <input ref={input1Ref} className="border-2" type="text" />
+        <button className="border-2 px-1 mx-2" onClick={handleData1}>
+          Save
+        </button>
+        <br />
+        <br />
+        <input ref={input2Ref} className="border-2" type="text" />
+        <button className="border-2 px-1 mx-2" onClick={handleData2}>
+          Save
+        </button>
         <br />
         <div className="flex gap-3 my-2">
-          <button className='border-2 px-1 mx-2' onClick={handleUndo}>Undo</button>
-          <button className='border-2 px-1 mx-2' onClick={handleRedo}>Redo</button>
+          <button className="border-2 px-1 mx-2" onClick={handleUndo}>
+            Undo
+          </button>
+          <button className="border-2 px-1 mx-2" onClick={handleRedo}>
+            Redo
+          </button>
         </div>
-        <div className='flex gap-3 my-3 flex-wrap'>
-          {data.map(item => (
-            <div key={item.id} className='border-2 px-1 mx-2'>
+        <div className="flex gap-3 my-3 flex-wrap">
+          {data.map((item) => (
+            <div key={item.id} className="border-2 px-1 mx-2">
               {item.data1 || item.data2}
             </div>
           ))}
--- urdemo/src/app/page.js
+++ urdemo/src/app/page.js
@@ -1,122 +1,146 @@
-"use client"
-import React,{useEffect, useState} from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [data, setData] = useState([
     {
       id: 0,
-      data1: "data1"
+      data1: "data1",
     },
     {
       id: 1,
-      data1: "data2"
+      data1: "data2",
     },
     {
       id: 2,
-      data2: "data3"
-    }
-  ])
-  const [undo, setUndo] = useState("")
-  const [data1, setData1] = useState("")
-  const [data2, setData2] = useState("")
+      data2: "data3",
+    },
+  ]);
+  const [undo, setUndo] = useState("");
+  const [data1, setData1] = useState("");
+  const [data2, setData2] = useState("");
 
-  const [udoCount, setUdoCount] = useState(data.length)
+  const [udoCount, setUdoCount] = useState(data.length);
 
-  useEffect(() => { 
-    setUdoCount(data.length-1)
-  }
-  , [data])
+  useEffect(() => {
+    setUdoCount(data.length);
+  }, [data]);
 
   console.log(data);
 
-
   function handleData1() {
-    if(data1===""){
-      alert("This field is required.")
-    }else{
-      setData(prevState => [...prevState, {
-        id: prevState.length,
-        data1: data1
-      }])
-      console.log("else data1",data1);
+    if (data1 === "") {
+      alert("This field is required.");
+    } else {
+      setData((prevState) => [
+        ...prevState,
+        {
+          id: prevState.length,
+          data1: data1,
+        },
+      ]);
+      console.log("else data1", data1);
     }
-  console.log("fun data1",data1);
-    setData2("")
+    console.log("fun data1", data1);
+    setData2("");
   }
 
   function handleData2() {
-    if(data2===""){
-      alert("This field is required.")
-    }else{
-      setData(prevState => [...prevState, {
-        id: prevState.length,
-        data2: data2
-      }])
-      console.log("else data2",data2);
+    if (data2 === "") {
+      alert("This field is required.");
+    } else {
+      setData((prevState) => [
+        ...prevState,
+        {
+          id: prevState.length,
+          data2: data2,
+        },
+      ]);
+      console.log("else data2", data2);
     }
-  console.log("fun data2",data2);
-    setData1("")
+    console.log("fun data2", data2);
+    setData1("");
   }
 
   function handleUndo() {
-    console.log("us",udoCount);
-    var count = udoCount - 1
-    if(count>=0){
-      setUdoCount(count)
-      if(data[`${count}`].data1){
-        setData1(data[`${udoCount}`].data1)
-        setData2("")
-      }else{
-        setData2(data[`${udoCount}`].data2)
-        setData1("")
+    console.log("us", udoCount);
+    var count = udoCount - 1;
+    if (count >= 0) {
+      setUdoCount(count);
+      if (data[`${count}`].data1) {
+        setData1(data[`${udoCount}`].data1);
+        console.log("Undo", data[`${count}`].data1);
+        setData2("");
+      } else {
+        setData2(data[`${udoCount}`].data2);
+        console.log("Undo", data[`${count}`].data2);
+        setData1("");
       }
     }
-    
-    console.log("ue",udoCount);
+
+    console.log("ue", udoCount);
   }
 
-  function handleRedo() { 
-    console.log("rs",udoCount); 
-    var count = udoCount + 1
-    if(count<data.length){
-      setUdoCount(count)
-      if(data[`${count}`].data1){
-        setData1(data[`${udoCount}`].data1)
-        setData2("")
-      }else{
-        setData2(data[`${udoCount}`].data2)
-        setData1("")
+  function handleRedo() {
+    console.log("rs", udoCount);
+    var count = udoCount + 1;
+    if (count < data.length) {
+      setUdoCount(count);
+      if (data[`${count}`].data1) {
+        setData1(data[`${udoCount}`].data1);
+        console.log("Redo", data[`${count}`].data1);
+        setData2("");
+      } else {
+        setData2(data[`${udoCount}`].data2);
+        console.log("Redo", data[`${count}`].data2);
+        setData1("");
       }
     }
-    console.log("re",udoCount); 
+    console.log("re", udoCount);
   }
-
-  
 
   return (
     <div>
-        <div className='mt-20 px-30'>
-        <input className='border-2' value={data1} type='text' onChange={(e)=>setData1(e.target.value)}></input> 
-        <button className='border-2 px-1 mx-2' onClick={()=>handleData1()}>save</button>
-         <br></br>
+      <div className="mt-20 px-30">
+        <input
+          className="border-2"
+          value={data1}
+          type="text"
+          onChange={(e) => setData1(e.target.value)}
+        ></input>
+        <button className="border-2 px-1 mx-2" onClick={() => handleData1()}>
+          save
+        </button>
         <br></br>
-        <input className='border-2' type='text' onChange={(e)=>setData2(e.target.value)}></input>
-        <button className='border-2 px-1 mx-2' onClick={()=>handleData2()}>save</button>
+        <br></br>
+        <input
+          className="border-2"
+          type="text"
+          onChange={(e) => setData2(e.target.value)}
+        ></input>
+        <button className="border-2 px-1 mx-2" onClick={() => handleData2()}>
+          save
+        </button>
         <br></br>
         <div className="flex gap-3 my-2">
-          <button className='border-2 px-1 mx-2' onClick={()=>handleUndo()}>Undo</button>
-          <button className='border-2 px-1 mx-2' onClick={()=>handleRedo()}>Redo</button>
+          <button className="border-2 px-1 mx-2" onClick={() => handleUndo()}>
+            Undo
+          </button>
+          <button className="border-2 px-1 mx-2" onClick={() => handleRedo()}>
+            Redo
+          </button>
         </div>
-        <div className='flex gap-3 my-3'>
+        <div className="flex gap-3 my-3">
           {data.map((item) => (
-            <div key={item.id} className='border-2 px-1 mx-2'>{item.data1}{item.data2}</div>
+            <div key={item.id} className="border-2 px-1 mx-2">
+              {item.data1}
+              {item.data2}
+            </div>
           ))}
         </div>
         {/* <p>undo = {redo}</p> */}
         <p>{undo}</p>
       </div>
-
     </div>
   );
 }
