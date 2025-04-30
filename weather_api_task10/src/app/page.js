2252f6ef03ac0ed9f673c7de8044c8fff7f34461
Ganganathen <ganganathan.m@outlook.com>
2025-04-30 18:41:08 +0530

    feat: add new background styles and update layout for improved UI; include new SVG assets

--- weather_api_task10/src/app/page.js
+++ weather_api_task10/src/app/page.js
@@ -32,8 +32,8 @@ export default function Home() {
   }, [cityName]);
 
   return (
-    <div className="w-full flex flex-row p-7 gap-4 text-white">
-      <div className="bg-col w-[10%] rounded-3xl p-5 flex flex-col shadow-sm">
+    <div className="w-full flex p-7 gap-4 text-white">
+      <div className="bg-col w-[7%] rounded-3xl p-5 shadow-sm">
         <div className="flex flex-col justify-center  items-center text-center gap-2">
           <Image
             src="/assets/main/bijli.svg"
@@ -68,17 +68,79 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-[90%]">
-        <div className="flex justify-between">
-          <input type="text" className="relative outline-none bg-col py-4 px-[5%] rounded-3xl shadow-sm w-[50%] text-secondery" placeholder="Search City..."/>
-          <div className="flex ">
-            <p>logo</p>
-            <p>username</p>
-            <p>arrow</p>
+      <div className="w-[93%]">
+        <div className="relative flex justify-between items-center">
+          <input
+            type="text"
+            className=" outline-none bg-col py-3 px-[7%] rounded-3xl shadow-sm w-[50%] text-secondery"
+            placeholder="Search City..."
+          />
+          <Image
+            className="absolute top[10%] left-[2%]"
+            src="/assets/user/search.svg"
+            width={25}
+            height={25}
+            alt="up.svg"
+          />
+          <div className="w-[30%] flex justify-between  bg-col rounded-3xl py-3 px-4">
+            <div className="flex justify-between gap-2">
+              <Image
+                className="img-bg rounded-4xl p-1"
+                src="/assets/user/up.svg"
+                width={25}
+                height={25}
+                alt="up.svg"
+              />
+              <p className="text-[14px]">username</p>
+            </div>
+
+            <Image
+              src="/assets/user/d-arrow.svg"
+              width={15}
+              height={15}
+              alt="up.svg"
+            />
           </div>
         </div>
-        <div>
-          div 2
+        <div className="flex justify-between w-fulln gap-7 mt-5">
+          <div className="w-1/2 bg-col p-4 rounded-3xl">
+            <div className="w-full flex justify-between ">
+              <div className="">
+                <div className="flex p-2 rounded-2xl justify-start img-bg">
+                <Image
+                  src="/assets/user/location.svg"
+                  width={15}
+                  height={15}
+                  alt="up.svg"
+                />
+                <p>india</p>
+                </div>
+
+                <h2 className="text-[40px]">Monday</h2>
+                <p className="text-[16px]">24 Dec, 2023</p>
+
+                <h1 className="text-[64px]">26 * C</h1>
+                <p>High: 27 Low: 10</p>
+              </div>
+              <div className="w-[50%]">2</div>
+            </div>
+            <div className="w-1/2 bg-col">
+              <div className="w-full flex justify-between">
+                <div className="w-[80%]">1</div>
+                <div className="w-[20%]">2</div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2 mt-3 flex flex-col gap-y-2 ">
+            <div className="sec-bg">div 3</div>
+            <div>div 4</div>
+            <div className="bg-col flex">
+              <div className="">div 1</div>
+              <div>div 2</div>
+              <div>div 3</div>
+              <div>div 4</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
